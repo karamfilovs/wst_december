b06e1829978b39ee5c4e163a01124e1ae76b8a33
Alex Karamfilov <karamfilovs@gmail.com>
2022-12-15 22:31:34 +0200

    Fixed

--- src/test/java/ItemPageTest.java
+++ src/test/java/ItemPageTest.java
@@ -1,6 +1,6 @@
 public class ItemPageTest {
 
-    //TODO: For homework implement the follow tests by using both api and ui methods
+    //TODO: For homework implement the following tests by using both api and ui methods
     //Test 1: Can search for items by name (via UI)
     //Test 2: Can search for items by price from to (via UI)
     //Test 3: Can delete item single/bulk (via UI)
